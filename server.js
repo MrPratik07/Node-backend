146a6d3db7f0d02ac930d0b0c60bec6255ca88d1
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-15 23:32:43 +0530

    post api

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express=require('express');
 const server = express();
-server.use('/static',express.static("./static"));
+server.use('/',express.static("./static"));
+server.use(express.json());
+// server.get('/',(request,response)=>{
+//     response.send("Hello World, learning Node js");
+// });
 
-server.get('/',(request,response)=>{
-    response.send("Hello World, learning Node js");
-});
 
+server.post('/login',(req,res) =>{
+    console.log(req.body);
+    res.send("Authenticated");    
+})
 
 
 server.listen(3000,()=> console.log("Server is Live"));
+
+
+
+// fetch("/login",{
+//     headers:{
+//         "content-type":"application/json"
+//     },
+//     method:"post",
+//     body:JSON.stringify({
+//         userName:"PRATIK",
+//         password : "12345"
+//     })
+// })

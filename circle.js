0a01f0e06d31b0213bc46f22fd5aad50f21079f1
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-17 22:55:49 +0530

    added mongo connection

--- circle.js
+++ circle.js
@@ -1,3 +1,3 @@
-const {PI} =Math
-exports.area=(r) => PI*r**2;
-exports.circumference = (r) => 2*PI*r;
+// const {PI} =Math
+// exports.area=(r) => PI*r**2;
+// exports.circumference = (r) => 2*PI*r;
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express=require('express');
 const server = express();
 server.use('/',express.static("./static"));
+const moongoose = require("mongoose");
 server.use(express.json());
+
+
+const connection = moongoose.createConnection(
+    "mongodb://localhost:27017/User_input",{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }
+)
+
+const inputSchema= new moongoose.Schema(
+    {any:{}},
+    {strict:false,collection:"login"}
+)
+
+const inputQuery = connection.model("login",inputSchema);
+
+
 // server.get('/',(request,response)=>{
 //     response.send("Hello World, learning Node js");
 // });
